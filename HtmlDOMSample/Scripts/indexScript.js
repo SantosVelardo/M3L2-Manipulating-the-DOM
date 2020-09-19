36051b0f2bd42b2ac8809ce7f305983d12bbc9d1
Santos Velardo <santosvmorgado@hotmail.com>
2020-09-19 15:08:26 +0200

    Agregar archivos de proyecto.

---
+++ HtmlDOMSample/Scripts/indexScript.js
@@ -0,0 +1,36 @@
+﻿document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('newHobbyBtn').addEventListener('click', addHobbies);
+});
+
+function addHobbies() {
+
+    const inputList = document.querySelectorAll('.hobbiesInput');
+
+    if (inputList.length < 5) {
+
+        const hobbiesList = document.getElementById('hobbiesList');
+        const newLineElement = createNode('br'),
+            inputElement = createNode('input');
+
+        inputElement.setAttribute("class", "hobbiesInput");
+
+        append(hobbiesList, newLineElement);
+        append(hobbiesList, inputElement);
+    }
+    else {
+        document.getElementById('newHobbyBtn').removeEventListener('click', addHobbies);
+    }
+}
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+
+
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Module  3: Introduction to JavaScript
+
+## Lesson 2 Demonstration: Manipulating the DOM with Javascript
+
+
+
+Creamos un archivo JavaScript externo a la página web en el que variaremos el formulario existente para poder introducir distinto número de datos, hasta un límite.
+
+
+
